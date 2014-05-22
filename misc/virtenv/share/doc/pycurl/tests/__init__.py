c8ccf55c9aab5e9f7bd33c13038de5a8f21db76b
Rafael Caricio <rafael.jacinto@gmail.com>
2014-05-22 17:24:39 +0200

    Remove unused stuff

--- misc/virtenv/share/doc/pycurl/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-def setup_package():
-    # import here, not globally, so that running
-    # python -m tests.appmanager
-    # to launch the app manager is possible without having pycurl installed
-    # (as the test app does not depend on pycurl)
-    import pycurl
-    
-    print('Testing %s' % pycurl.version)
